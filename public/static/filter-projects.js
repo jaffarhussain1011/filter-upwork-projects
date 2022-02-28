8411674321a592147333d69302288bb27ff79345
Jaffar Hussain <jaffarhussain1011@gmail.com>
2022-02-28 15:35:23 +0500

    added projects hide show feature based on toggling of country switch

--- public/static/filter-projects.js
+++ public/static/filter-projects.js
@@ -1,9 +1,21 @@
 chrome.runtime.onMessage.addListener(
-     (request, sender, sendResponse) => {
+    (request, sender, sendResponse) => {
         if (request.action === "getCountryList") {
             let countries = [...document.querySelectorAll("[data-test='client-country']")].map(el => el.textContent.trim())
             countries = [...new Set(countries)];
-            chrome.runtime.sendMessage({countries: countries});
+            chrome.runtime.sendMessage({ countries: countries });
+        }
+        if (request.action === "filterProjects") {
+            // console.log(request)
+            [...document.querySelectorAll("[data-test='client-country']")].map(el => {
+                if (el.textContent.trim() == request.country) {
+                    if (request.checked) {
+                        el.closest(".up-card-section").style.display = "block";
+                    } else {
+                        el.closest(".up-card-section").style.display = "none";
+                    }
+                }
+            })
         }
     }
 );
--- src/App.js
+++ src/App.js
@@ -1,50 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
-import React from 'react';
+import React from "react";
+import { Container, Typography, Button, FormControlLabel, Switch } from "@mui/material";
+import { Grid, List, ListItem, ListItemText, IconButton } from "@mui/material";
+import LinearProgress from '@mui/material/LinearProgress';
+
+
 class App extends React.Component {
+
   constructor(props) {
     super(props)
     this.state = {
       countries: [],
-      upwork:  false
+      upwork: false,
+      isLoading: true
     }
   }
+
+  handleChange = (event) => {
+    this.filterProjects({ country: event.target.value, checked: event.target.checked });
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          Countries List From Current Page
-          <button onClick={this.filterProjects}>
-            Retreive
-          </button>
-          <div >
-            <ul>
-              {this.state.countries.map(country => <li>{country}</li>)}
-            </ul>
-          </div>
-        </header>
+        <Container>
+          {(!this.state.upwork) && <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div">
+            Works only on upwork tab
+
+          </Typography>}
+          {this.state.upwork && <Grid item xs={12} md={6}>
+            <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div">
+              Countries List From Current Page
+
+            </Typography>
+            {this.state.isLoading && <div>
+              <LinearProgress color="secondary" />
+              <LinearProgress color="success" />
+              <LinearProgress color="inherit" />
+            </div>}
+            <List >
+              {this.state.countries.map(country =>
+                <ListItem
+                  secondaryAction={
+                    <IconButton edge="end" aria-label="delete">
+                      <Switch value={country} defaultChecked onChange={this.handleChange} />
+                    </IconButton>
+                  }
+                >
+                  <ListItemText
+                    primary={country}
+                  />
+                </ListItem>
+              )}
+            </List>
+          </Grid>}
+
+        </Container>
       </div>
     );
   }
   handleMessage(response) {
     // Handle received messages
     if (response.countries) {
-      this.setState({ countries: response.countries });
+      this.setState({ countries: response.countries, isLoading: false });
     }
+
   }
   componentDidMount() {
+    // check if its upwork url
+    window.chrome.tabs.query({ active: true, currentWindow: true }, ([currentTab]) => {
+      const pattern = /upwork\.com/
+      if (pattern.test(currentTab.url)) {
+        this.setState({ upwork: true });
+      }
+
+    })
     // Add listener when component mounts
+    this.getCountryList()
     window.chrome.runtime.onMessage.addListener(this.handleMessage.bind(this));
   }
   componentWillUnmount() {
     // Remove listener when this component unmounts
     window.chrome.runtime.onMessage.removeListener(this.handleMessage);
   }
-  filterProjects() {
-    window.chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      var activeTab = tabs[0];
-      console.log(activeTab);
-      window.chrome.tabs.sendMessage(activeTab.id, { "action": "getCountryList" })
+  getCountryList() {
+    window.chrome.tabs.query({ active: true, currentWindow: true }, ([currentTab]) => {
+      window.chrome.tabs.sendMessage(currentTab.id, { "action": "getCountryList" })
+    });
+  }
+
+  filterProjects(data) {
+    window.chrome.tabs.query({ active: true, currentWindow: true }, ([currentTab]) => {
+      window.chrome.tabs.sendMessage(currentTab.id, { ...data, "action": "filterProjects" })
     });
   }
 }
