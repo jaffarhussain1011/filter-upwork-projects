6b4ccb0c77a0925383fd0c4b9b1265f1da754626
Jaffar Hussain <jaffarhussain1011@gmail.com>
2022-03-01 15:52:39 +0500

    added for search page

--- public/static/filter-projects.js
+++ public/static/filter-projects.js
@@ -1,18 +1,19 @@
 chrome.runtime.onMessage.addListener(
     (request, sender, sendResponse) => {
         if (request.action === "getCountryList") {
-            let countries = [...document.querySelectorAll("[data-test='client-country']")].map(el => el.textContent.trim())
+            let countries = [...document.querySelectorAll("[data-test='client-country'],.client-location")].map(el => el.textContent.trim())
             countries = [...new Set(countries)];
             chrome.runtime.sendMessage({ countries: countries });
         }
         if (request.action === "filterProjects") {
             // console.log(request)
-            [...document.querySelectorAll("[data-test='client-country']")].map(el => {
+            [...document.querySelectorAll("[data-test='client-country'],.client-location")].map(el => {
                 if (el.textContent.trim() == request.country) {
                     if (request.checked) {
-                        el.closest(".up-card-section").style.display = "block";
+                        // [data-job-tile='::job']
+                        el.closest(".up-card-section,.job-tile").style.display = "block";
                     } else {
-                        el.closest(".up-card-section").style.display = "none";
+                        el.closest(".up-card-section,.job-tile").style.display = "none";
                     }
                 }
             })
